5d4afdf98ca941c743a66dc931baf4b4f2043f1f
bogs212000 <garryearllontes21@gmail.com>
2025-01-03 21:33:49 +0800

    add animations

--- lib/src/screen/loading.screen.dart
+++ lib/src/screen/loading.screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -15,13 +16,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: VxBox(
-              child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [],
-      ))
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: CupertinoActivityIndicator(
+                radius: 15,
+                color: CupertinoColors.white,
+              ),
+            )
+          ],
+        ),
+      )
           .height(MediaQuery.of(context).size.height)
           .width(MediaQuery.of(context).size.width)
-          .bgImage(DecorationImage(image: AssetImage(appImage.bg), fit: BoxFit.cover))
+          .bgImage(DecorationImage(
+              image: AssetImage(appImage.bg), fit: BoxFit.cover))
           .make(),
     );
   }
